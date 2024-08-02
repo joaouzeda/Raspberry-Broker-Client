45c9d400eb8c067dc309f8a21de6109c0e159e19
João Uzêda <uzeda.joao.p@gmail.com>
2024-08-02 17:24:40 -0300

    v0.0.1

---
+++ application/main/main.py
@@ -0,0 +1,15 @@
+import time
+from application.configs.broker_configs import mqtt_broker_configs
+from mqtt_connection.mqtt_client_connection import MqttClientConnection
+
+def start():
+    mqtt_client_connetcion = MqttClientConnection(
+        mqtt_broker_configs["HOST"],
+        mqtt_broker_configs["PORT"],
+        mqtt_broker_configs["CLIENT_NAME"],
+        mqtt_broker_configs["KEPPALIVE"]
+    )
+    mqtt_client_connetcion.start_connection()
+
+    while True: time.sleep(0.001)
+         
